6fdd1f6d128af82f4f790c00104220b36ce376ee
Maxim Mitin <maximus405@gmail.com>
2021-07-07 22:11:26 +0300

    Initial Commit

---
+++ HW_2.1.2_trafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_2.1.2_trafficLight
+//
+//  Created by Maxim Mitin on 7.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
