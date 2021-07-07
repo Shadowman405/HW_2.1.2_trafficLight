53eb32524dd4595c68a4f71236727c5c0666f957
Maxim Mitin <maximus405@gmail.com>
2021-07-07 23:34:47 +0300

    ready for checking

--- HW_2.1.2_trafficLight/ViewController.swift
+++ HW_2.1.2_trafficLight/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redViewLbl: UIView!
+    @IBOutlet weak var yellowViewLbl: UIView!
+    @IBOutlet weak var greenViewLbl: UIView!
+    
+    @IBOutlet weak var buttonLbl: UIButton!
+    
+    enum trafficLights {
+        case red, yellow , green
+    }
+    var activeLight : trafficLights = .red
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redViewLbl.layer.cornerRadius = 90
+        yellowViewLbl.layer.cornerRadius = 90
+        greenViewLbl.layer.cornerRadius = 90
+        
+        redViewLbl.backgroundColor = .red.withAlphaComponent(0.1)
+        yellowViewLbl.backgroundColor = .yellow.withAlphaComponent(0.1)
+        greenViewLbl.backgroundColor = .green.withAlphaComponent(0.1)
+    }
+    
+    func switchLight() {
+        switch activeLight{
+        case .red:
+            redViewLbl.backgroundColor = .red.withAlphaComponent(0.9)
+            yellowViewLbl.backgroundColor = .yellow.withAlphaComponent(0.1)
+            greenViewLbl.backgroundColor = .green.withAlphaComponent(0.1)
+            activeLight = .yellow
+        case .yellow:
+            yellowViewLbl.backgroundColor = .yellow.withAlphaComponent(0.9)
+            redViewLbl.backgroundColor = .red.withAlphaComponent(0.1)
+            greenViewLbl.backgroundColor = .green.withAlphaComponent(0.1)
+            activeLight = .green
+        case .green:
+            greenViewLbl.backgroundColor = .green.withAlphaComponent(0.9)
+            yellowViewLbl.backgroundColor = .yellow.withAlphaComponent(0.1)
+            redViewLbl.backgroundColor = .red.withAlphaComponent(0.1)
+            activeLight = .red
+        }
     }
 
-
+    @IBAction func buttonDidClick(_ sender: Any) {
+        buttonLbl.setTitle("Next", for: .normal)
+        
+        switchLight()
+    
+ }
 }
-
